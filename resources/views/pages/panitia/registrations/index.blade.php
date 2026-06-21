56c2589dd6e97c7510f66e2ee19eda5cfec83a7c
TracerStudySmekdapapa <apolloastro322@gmail.com>
2026-06-21 18:54:25 +0700

    Update index.blade.php

--- resources/views/pages/panitia/registrations/index.blade.php
+++ resources/views/pages/panitia/registrations/index.blade.php
@@ -32,13 +32,13 @@
         </x-slot:action>
 
         @if (session('success'))
-            <div class="mx-5 mt-4 rounded-lg bg-green-50 px-4 py-3 text-sm text-green-700">
+            <div class=" mb-4 rounded-lg bg-green-50 px-4 py-3 text-sm text-green-700">
                 {{ session('success') }}
             </div>
         @endif
 
         @if (session('error'))
-            <div class="mx-5 mt-4 rounded-lg bg-red-50 px-4 py-3 text-sm text-red-700">
+            <div class=" mb-4 rounded-lg bg-red-50 px-4 py-3 text-sm text-red-700">
                 {{ session('error') }}
             </div>
         @endif
